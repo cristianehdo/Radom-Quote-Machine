6e31c9f4175f6e6c94c4bf02c7d47e3c922af36a
Cristiane Hdo <cristiane.hdo@gmail.com>
2017-04-25 17:43:52 -0300

    refactured function random quote and create fuction tweetshare

--- static/js/script.js
+++ static/js/script.js
@@ -2,40 +2,19 @@
 
 
 
- function randomFunction(){
+function randomFunction(){
+  var quotes = ["C'est quand le silence s'est tu que les oiseaux se sont mis à chanter.", "On aurait voulu choisir la facilité mais c'était trop compliqué.", "J'habite une maison en moi...", "La lune n'éclaire que les yeux ouverts", "C'est parce que le feu est sacré qu'on ne laissera pas les rivières l'emporter.", "Sans la magie des femmes nous serions noirs comme nos nuits.", "Cent paroles ne valent pas une flamme.", "Nouvelle aube....même si elle n'existe pas on fera comme si elle était là.", "Pour la victoire de la lumière nous brandirons une torche sur les plus longs chemins.", "S'il fait souvent noir dans nos histoires c'est que l'envie appartient `a la nuit."]
+  var num = Math.floor((Math.random() * (quotes.length-1)) + 1);
+  document.getElementById("showQuote").innerText = quotes[num];
+}
+function tweetFunction(){
+  var textShow = document.getElementById("showQuote").innerText;
+  window.open("https://twitter.com/intent/tweet?text="+ textShow);
 
-    var number = Math.floor((Math.random() * 10) + 1);
-    var answer = randomQuotes(number);
-    document.getElementById("showQuote").innerText = answer;
+}
 
-  }
 
-function randomQuotes(num) {
-  var answer = "";
-  switch (num) {
-      case 1: answer = "C'est quand le silence s'est tu que les oiseaux se sont mis à chanter.";
-      break;
-      case 2: answer = "On aurait voulu choisir la facilité mais c'était trop compliqué.";
-      break;
-      case 3: answer = "J'habite une maison en moi...";
-      break;
-      case 4: answer = "La lune n'éclaire que les yeux ouverts";
-      break;
-      case 5: answer = "C'est parce que le feu est sacré qu'on ne laissera pas les rivières l'emporter.";
-      break;
-      case 6: answer = "Sans la magie des femmes nous serions noirs comme nos nuits.";
-      break;
-      case 7: answer = "Cent paroles ne valent pas une flamme.";
-      break;
-      case 8: answer = "Nouvelle aube....même si elle n'existe pas on fera comme si elle était là.";
-      break;
-      case 9: answer = "Pour la victoire de la lumière nous brandirons une torche sur les plus longs chemins.";
-      break;
-      case 10: answer = "case 10";
-    }
-      return answer;
-
-}  
+ 
 
 
   
